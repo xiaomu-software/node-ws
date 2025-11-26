3ad54b154cd4256a882f9cab7efb43baf66d57e1
xiaomu <1393371997@qq.com>
2025-11-26 11:25:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -339,24 +339,7 @@ uuid: ${UUID}`;
 }; 
 
 async function addAccessTask() {
-  if (!AUTO_ACCESS) return;
-
-  if (!DOMAIN) {
-    return;
-  }
-  const fullURL = `https://${DOMAIN}`;
-  try {
-    const res = await axios.post("https://oooo.serv00.net/add-url", {
-      url: fullURL
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    console.log('Automatic Access Task added successfully');
-  } catch (error) {
-    // console.error('Error adding Task:', error.message);
-  }
+ return;
 }
 
 const delFiles = () => {
